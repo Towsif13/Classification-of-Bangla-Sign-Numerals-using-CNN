6c94a37e11d220279c930835a11fefbc5bb47f9b
Towsif13 <towsifc@gmail.com>
2021-03-19 20:22:27 +0600

    updates

--- data.py
+++ data.py
@@ -5,9 +5,9 @@ from torchvision.transforms import ToTensor
 
 
 # directories
-TRAIN_DIR = 'C:/Users/Towsif/Desktop/Files/BdSL/BdSL_digits/split/train'
-VAL_DIR = 'C:/Users/Towsif/Desktop/Files/BdSL/BdSL_digits/split/val'
-TEST_DIR = 'C:/Users/Towsif/Desktop/Files/BdSL/BdSL_digits/split/test'
+TRAIN_DIR = '...Files/BdSL/BdSL_digits/split/train'
+VAL_DIR = '...Files/BdSL/BdSL_digits/split/val'
+TEST_DIR = '...Files/BdSL/BdSL_digits/split/test'
 
 
 train_data = ImageFolder(TRAIN_DIR, transform=ToTensor())
